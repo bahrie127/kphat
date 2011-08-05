a668ad189022da6ab9fe51dd1efd2aca0dc28814
^_^ <phepen@Phepen.(none)>
2011-08-05 23:23:49 +0700

    pedaftaran

--- application/views/admin2/admin2view/acaraView/showtablependaftaran.php
+++ application/views/admin2/admin2view/acaraView/showtablependaftaran.php
@@ -7,8 +7,7 @@
                     <th>Event</th>
                     <th>Date</th>
                     
-                    <th>Update | Delete</th> 
-
+                  
                 </tr> 
             </thead> 
     <tbody> 
@@ -16,18 +15,78 @@
         $i = 0;
         foreach ($data as $row) {
             $i++;
-            echo "<tr class=\"record\">";
+            echo "<tr>";
             echo "<td>$i</td>";
             echo "<td>$row->pendaftaran_codependaftaran</td>";
             echo "<td>$row->user_nama</td>";
             echo "<td>$row->event_namaevent</td>";
             echo "<td>$row->pendaftaran_tanggal</td>";
             
-            echo "<td align=\"center\"><a href=\"#\" class=\"editE\" codeEvent=\"#\" namaevent=\"#\" materi=\"#\" tiny_input=\"#\">Edit</a>";
-            
-            echo "</td></tr>";
+           
+            echo "</tr>";
         }
         ?>
 
     </tbody> 
 </table>
+
+<script text="javascript">
+    $(document).ready(function() {
+        // DataTables Config (more info can be found at http://www.datatables.net/)
+        var oTable = $('.datatable').dataTable( {
+            "bJQueryUI": true,
+            "sScrollX": "",
+            "bSortClasses": false,
+            "aaSorting": [[0,'asc']],
+            "bAutoWidth": true,
+            "bInfo": true,
+            "sScrollY": "100%",	
+            "sScrollX": "100%",
+            "bScrollCollapse": true,
+            "sPaginationType": "full_numbers",
+            "bRetrieve": true
+        } );
+
+	
+
+
+        // DataTables Config - this reloads dataTables when it is placed in a hidden tab			
+	
+        $(".tabs").tabs( {
+            "select": function(event, ui) {
+                oTable.fnAdjustColumnSizing();
+                $('.tabs div.dataTables_scroll').css({
+                    "visibility":"hidden" 
+                });
+            },
+            "show": function(event, ui) {
+                var oTable = $('div.dataTables_scrollBody > table', ui.panel).dataTable();
+                if ( oTable.length > 0 ) {
+                    oTable.fnAdjustColumnSizing();
+                    $(".tabs div.dataTables_scroll").css({
+                        "display":"none",
+                        "visibility":"visible" 
+                    }).show();
+                }
+            }
+        } );
+
+        // DataTables Config - this reloads dataTables when it is placed in a closed accordion			
+        $( ".content_accordion" ).accordion( {
+            "change": function(event, ui) {
+                var oTable = $('div.dataTables_scrollBody > table', ui.panel).dataTable();
+                if ( oTable.length > 0 ) {
+                    oTable.fnAdjustColumnSizing();
+                    $(".content_accordion div.dataTables_scroll").css({
+                        "display":"none",
+                        "visibility":"visible" 
+                    }).show();
+                }
+            }
+        });
+    
+        $(window).resize(function(){
+            oTable.fnAdjustColumnSizing();
+        });
+    });
+</script>
