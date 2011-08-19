b481b2c711c36c8a0ceed58271b284f905884a36
^_^ <phepen@Phepen.(none)>
2011-08-19 13:05:11 +0700

    pembukuan

--- application/views/admin2/admin2view/acaraView/total_pemasukan.php
+++ application/views/admin2/admin2view/acaraView/total_pemasukan.php
@@ -1,17 +1,37 @@
+
+
 <?php foreach ($masuk as $msk): ?>
     <?php $pemasukan = $msk->masuk; ?>
-        
+
 <?php endforeach; ?>
 
 <?php foreach ($keluar as $klr): ?>
     <?php $pengeluaran = $klr->keluar; ?>
-        
 
-    <table>
+
+    <table class="tt">
         <tr>
-            <td><h3>Jumlah Pemasukan: </h3></td>
-            <td>&nbsp;&nbsp;</td>
-            <td><h3><?php echo $pemasukan - $pengeluaran; ?></h3></td>
+            <td><h3>Pemasukan</h3></td>
         </tr>
+        <tr>
+            <td><li>Coba</li></td>
+    <td></td>
+    <td><p>Harga</p></td>
+    </tr>
+    
+    <tr><br/><br/>
+        <td><h3>Pengeluaran</h3></td>
+    </tr>
+    <tr>
+        <td><li>Coba</li></td>
+    <td></td>
+    <td><p>Harga</p></td>
+    </tr>
+    <tr>
+
+        <td><h3>Jumlah Pemasukan: </h3></td>
+        <td></td>
+        <td><h3><?php echo $pemasukan - $pengeluaran; ?></h3></td>
+    </tr>
     </table>
 <?php endforeach; ?>
